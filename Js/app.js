1784917ec68602830c59a08ea77a0048e7b72b9b
Luis Manfredi <75541418+Luis-Manfredi@users.noreply.github.com>
2021-06-03 15:12:09 -0400

    Update app.js

--- Js/app.js
+++ Js/app.js
@@ -5,8 +5,11 @@ function sumar(){
     let operandoA = forma['operandoA'];
     let operandoB = forma['operandoB'];
     let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
+    
+//  In case no numbers added:
     if (isNaN(resultado))
         resultado = 'La operación no incluye números';
+    
     document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
     console.log(`Resultado: ${resultado}`);
 }
